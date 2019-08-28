ccbc4b53e99663f650ae6521ff2c3bb8a7e7391b
Preston Van <sophinvanpreston@gmail.com>
2019-08-28 11:35:49 -0400

    test

--- src/main/java/edu/clayton/csit/antlab/person/Person2.java
+++ src/main/java/edu/clayton/csit/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.clayton.csit.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Qu
  * @version 1.1
@@ -38,7 +38,7 @@ public class Person2 {
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
